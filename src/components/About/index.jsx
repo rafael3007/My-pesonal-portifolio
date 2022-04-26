2d70571885bfb80bec0991de7c87cc4b494fbf2d
Rafael Brito <rafael.brito.14222@gmail.com>
2022-04-26 13:22:54 -0300

    about icon - first child css problem

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
-import "./styles"
+import { Section,AboutContainer,AboutMe,AboutMeImg,AboutCards } from "./styles"
+
+import ME from "../../assets/me-about.jpg"
+import { FaAward } from "react-icons/fa"
+import { FiUsers } from "react-icons/fi"
+import { VscFolderLibrary } from "react-icons/vsc"
 
 const About = () => {
   return (
-    <section id="about">Sobre</section>
+    <Section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <AboutContainer className="container ">
+        <AboutMe>
+          <AboutMeImg>
+            <img src={ME} alt="About Image" />
+          </AboutMeImg>
+        </AboutMe>
+
+        <div className="about__content">
+          <AboutCards>
+            <article className='about__card'>
+              <FaAward className='about_icon'/>
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+            <article className='about__card'>
+              <FiUsers className='about_icon'/>
+              <h5>Clients</h5>
+              <small>200+ WorldWide</small>
+            </article>
+            <article className='about__card'>
+              <VscFolderLibrary className='about_icon'/>
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </AboutCards>
+
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae iste facere suscipit molestiae cum, tenetur enim dolor dolores adipisci exercitationem nulla aliquam tempore eius ex totam, nemo vitae officia cupiditate.
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        
+        </div>
+      </AboutContainer>
+    </Section>
   )
 }
 
--- src/components/About/styles.js
+++ src/components/About/styles.js
@@ -1,6 +1,63 @@
 import styled from "styled-components";
 
-const Div = styled.div`
+export const Section = styled.section`
+
+    
+`
+
+
+export const AboutContainer = styled.div`
+    display: grid;
+    grid-template-columns: 35% 50%;
+    gap: 15%;
+`
+
+export const AboutMe = styled.div`
+    width: 100%;
+    aspect-ratio: 1/1;
+    border-radius: 2rem;
+    background: linear-gradient(
+        45deg, transparent,
+        var(--color-primary),
+        transparent
+    );
+    display: grid;
+    place-items: center;
 
 `
-export default Div;
+
+export const AboutMeImg = styled.div`
+    border-radius: 2rem;
+    overflow: hidden;
+    transform: rotate(10deg);
+    transition: var(--transition);
+    
+    :hover {
+        transform: rotate(0) 
+    }
+`
+
+export const AboutCards = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 1.5rem;
+
+    article {
+        background: var(--color-bg-variant);
+        border: 1px solid transparent;
+        border-radius: 1rem;
+        padding: 2rem;
+        text-align: center;
+        transition: var(--transition);
+    }
+
+    article:hover {
+        background: transparent;
+        border-color: var(--color-primary-variant);
+        cursor: default;
+    }
+    
+`
+
+
+
