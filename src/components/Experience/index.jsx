350f6609e4071a6ece0e36c4f4b97d7c124f3230
rafael3007 <rafael.brito.1422@gmail.com>
2022-04-27 13:17:42 -0300

    responsivity of Experience

--- src/components/Experience/index.jsx
+++ src/components/Experience/index.jsx
@@ -1,9 +1,103 @@
 import React from 'react'
-import "./styles"
+import { ExperienceContainer,ExperienceBox, ExperienceContent, ExperienceDetails}from "./styles"
+import { BsPatchCheckFill } from "react-icons/bs"
 
 const Experience = () => {
   return (
-    <section id="experience">Experience</section>
+    <section id="experience">
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <ExperienceContainer className="container">
+        <ExperienceBox>
+          <h3>Frontend Development</h3>
+          <ExperienceContent>
+
+            <ExperienceDetails>
+              <BsPatchCheckFill className='icon'/>
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </ExperienceDetails>
+
+            <ExperienceDetails>
+              <BsPatchCheckFill className='icon'/>
+              <div>
+                <h4>Javascript</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </ExperienceDetails>
+
+            <ExperienceDetails>
+              <BsPatchCheckFill className='icon'/>
+              <div>
+                <h4>Bootstrap</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </ExperienceDetails>
+
+            <ExperienceDetails>
+              <BsPatchCheckFill className='icon'/>
+              <div>
+                <h4>Tailwind</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </ExperienceDetails>
+
+            <ExperienceDetails>
+              <BsPatchCheckFill className='icon'/>
+              <div>
+                <h4>React</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </ExperienceDetails>
+
+            
+
+          </ExperienceContent>
+        </ExperienceBox>
+        <ExperienceBox>
+          <h3>Backend Development</h3>
+            <ExperienceContent>
+
+              <ExperienceDetails>
+                <BsPatchCheckFill className='icon'/>
+                <div>
+                  <h4>NodeJS</h4>
+                  <small className='text-light'>Experienced</small>
+                </div>
+              </ExperienceDetails>
+
+              <ExperienceDetails>
+                <BsPatchCheckFill className='icon'/>
+                <div>
+                  <h4>MySql</h4>
+                  <small className='text-light'>Experienced</small>
+                </div>
+              </ExperienceDetails>
+
+              <ExperienceDetails>
+                <BsPatchCheckFill className='icon'/>
+                <div>
+                  <h4>Express</h4>
+                  <small className='text-light'>Experienced</small>
+                </div>
+              </ExperienceDetails>
+
+              <ExperienceDetails>
+                <BsPatchCheckFill className='icon'/>
+                <div>
+                  <h4>MongoDD</h4>
+                  <small className='text-light'>Experienced</small>
+                </div>
+              </ExperienceDetails>           
+
+            </ExperienceContent>
+
+        </ExperienceBox>
+      </ExperienceContainer>
+    </section>
   )
 }
 
--- src/components/Experience/styles.js
+++ src/components/Experience/styles.js
@@ -1,6 +1,48 @@
 import styled from "styled-components";
 
-const Div = styled.div`
+export const ExperienceContainer = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 2rem;
 
 `
-export default Div;
+
+export const ExperienceBox = styled.div`
+    background: var(--color-bg-variant);
+    padding: 2.4rem 5rem;
+    border-radius: 2rem;
+    border: 1px solid transparent;
+    transition: var(--transition);
+
+    :hover{
+        background: transparent;
+        border-color: var(--color-primary-variant);
+        cursor: default;
+    }
+
+    h3 {
+        text-align: center;
+        margin-bottom: 2rem;
+        color: var(--color-primary)
+    }
+
+`
+
+export const ExperienceContent = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    row-gap: 2rem;
+
+`
+
+export const ExperienceDetails = styled.article`
+    display: flex;
+    gap: 1rem;
+
+    .icon {
+        margin-top: 6px;
+        color: var(--color-primary);
+    }
+
+`
+
