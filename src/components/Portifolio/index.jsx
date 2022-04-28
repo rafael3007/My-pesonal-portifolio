a0c7310af0c2ed4c91970fbd63debd15a764645c
rafael3007 <rafael.brito.1422@gmail.com>
2022-04-28 13:04:11 -0300

    Portfolio style but not responsive

--- src/components/Portifolio/index.jsx
+++ src/components/Portifolio/index.jsx
@@ -1,9 +1,87 @@
-import React from 'react'
-import "./styles"
+import React from 'react';
+import { PortfolioButtons, PortifolioContainer, PortifolioItem, PortifolioItemImage, PortifolioSection } from './styles';
+import IMG1 from '../../assets/portfolio1.jpg';
+import IMG2 from '../../assets/portfolio2.jpg';
+import IMG3 from '../../assets/portfolio3.jpg';
+import IMG4 from '../../assets/portfolio4.jpg';
+import IMG5 from '../../assets/portfolio5.png';
+import IMG6 from '../../assets/portfolio6.jpg';
 
 const Portifolio = () => {
   return (
-    <div>Portifolio</div>
+    <PortifolioSection id="porifolio">
+      <h5>My Recent Work</h5>
+      <h2>Portifolio</h2>
+
+      <PortifolioContainer className='container'>
+        <PortifolioItem>
+          <PortifolioItemImage>
+            <img src={IMG1} alt="" />
+          </PortifolioItemImage>
+          <h3>This is a portfolio Item title</h3>
+          <PortfolioButtons>
+            <a href="https://github.com/rafael3007/" className='btn' target="_blank">GitHub</a>
+            <a href="https://dribbble.com/rafael3007/" className='btn btn-primary' target="_blank" >Live Demo</a>
+          </PortfolioButtons>
+        </PortifolioItem>
+
+        <PortifolioItem>
+          <PortifolioItemImage>
+            <img src={IMG2} alt="" />
+          </PortifolioItemImage>
+          <h3>This is a portfolio Item title</h3>
+          <PortfolioButtons>
+            <a href="https://github.com/rafael3007/" className='btn' target="_blank">GitHub</a>
+            <a href="https://dribbble.com/rafael3007/" className='btn btn-primary' target="_blank" >Live Demo</a>
+          </PortfolioButtons>
+        </PortifolioItem>
+
+        <PortifolioItem>
+          <PortifolioItemImage>
+            <img src={IMG3} alt="" />
+          </PortifolioItemImage>
+          <h3>This is a portfolio Item title</h3>
+          <PortfolioButtons>
+            <a href="https://github.com/rafael3007/" className='btn' target="_blank">GitHub</a>
+            <a href="https://dribbble.com/rafael3007/" className='btn btn-primary' target="_blank" >Live Demo</a>
+          </PortfolioButtons>
+        </PortifolioItem>
+
+        <PortifolioItem>
+          <PortifolioItemImage>
+            <img src={IMG4} alt="" />
+          </PortifolioItemImage>
+          <h3>This is a portfolio Item title</h3>
+          <PortfolioButtons>
+            <a href="https://github.com/rafael3007/" className='btn' target="_blank">GitHub</a>
+            <a href="https://dribbble.com/rafael3007/" className='btn btn-primary' target="_blank" >Live Demo</a>
+          </PortfolioButtons>
+        </PortifolioItem>
+
+        <PortifolioItem>
+          <PortifolioItemImage>
+            <img src={IMG5} alt="" />
+          </PortifolioItemImage>
+          <h3>This is a portfolio Item title</h3>
+          <PortfolioButtons>
+            <a href="https://github.com/rafael3007/" className='btn' target="_blank">GitHub</a>
+            <a href="https://dribbble.com/rafael3007/" className='btn btn-primary' target="_blank" >Live Demo</a>
+          </PortfolioButtons>
+        </PortifolioItem>
+
+        <PortifolioItem>
+          <PortifolioItemImage>
+            <img src={IMG6} alt="" />
+          </PortifolioItemImage>
+          <h3>This is a portfolio Item title</h3>
+          <PortfolioButtons>
+            <a href="https://github.com/rafael3007/" className='btn' target="_blank">GitHub</a>
+            <a href="https://dribbble.com/rafael3007/" className='btn btn-primary' target="_blank" >Live Demo</a>
+          </PortfolioButtons>
+        </PortifolioItem>
+      </PortifolioContainer>
+      
+    </PortifolioSection>
   )
 }
 
--- src/components/Portifolio/styles.js
+++ src/components/Portifolio/styles.js
@@ -1,6 +1,42 @@
 import styled from "styled-components";
 
-const Div = styled.div`
+export const PortifolioSection = styled.section`
 
 `
-export default Div;
+
+export const PortifolioContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 2.5rem;
+`
+
+export const PortifolioItem = styled.article`
+    background: var(--color-bg-variant);
+    padding: 1.3rem;
+    border-radius: 2rem;
+    border: 1px solid transparent;
+    transition: var(--transition);
+
+    :hover {
+        background: var(--color-primary-variant);
+        border-color: transparent;
+        
+    }
+
+    h3 {
+        margin: 1.2rem 0 2rem;
+        
+    }
+`
+
+export const PortfolioButtons = styled.div`
+    display: flex;
+    gap: 1rem;
+    margin-bottom: 1rem;
+`
+
+export const PortifolioItemImage = styled.div`
+    border-radius: 1.5rem;
+    overflow: hidden;
+
+`
