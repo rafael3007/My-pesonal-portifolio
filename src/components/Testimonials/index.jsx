0c4af359bab47a1aa95a3c6aac26ccbce8c9b230
rafael3007 <rafael.brito.1422@gmail.com>
2022-04-28 19:14:56 -0300

    slide testimonials

--- src/components/Testimonials/index.jsx
+++ src/components/Testimonials/index.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
-import "./styles"
+import React from 'react';
+import {ClientAvatar, Testimonial, TestimonialsContainer, TestimonialsSection } from './styles';
+
+import avatar1 from '../../assets/avatar1.jpg'
+import avatar2 from '../../assets/avatar2.jpg'
+import avatar3 from '../../assets/avatar3.jpg'
+import avatar4 from '../../assets/avatar4.jpg'
+
+const data = [
+  {
+    id: 1,
+    image: avatar1,
+    name: "João Pedro L. Santos",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt rerum magnam,excepturi ex exercitationem quos expedita praesentium reiciendis debitis dignissimos unde hic optio! Officia possimus reiciendis sint. Qui, optio rerum!"
+  },
+  {
+    id: 2,
+    image: avatar2,
+    name: "Felipe Da Silva",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt rerum magnam,excepturi ex exercitationem quos expedita praesentium reiciendis debitis dignissimos unde hic optio! Officia possimus reiciendis sint. Qui, optio rerum!"
+  },
+  {
+    id: 3,
+    image: avatar3,
+    name: "Rafael da Silva",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt rerum magnam,excepturi ex exercitationem quos expedita praesentium reiciendis debitis dignissimos unde hic optio! Officia possimus reiciendis sint. Qui, optio rerum!"
+  },
+  {
+    id: 4,
+    image: avatar4,
+    name: "Felipe",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt rerum magnam,excepturi ex exercitationem quos expedita praesentium reiciendis debitis dignissimos unde hic optio! Officia possimus reiciendis sint. Qui, optio rerum!"
+  }
+]
 
 const Testimonials = () => {
   return (
-    <section id="testimonials">Testimonials</section>
+    <TestimonialsSection id="testimonials">
+      <h5>Review from Clients</h5>
+      <h2>Testimonials</h2>
+
+      <TestimonialsContainer className="container">
+        {
+          data.map(({ image, name, description},index)=>{
+            return(
+              <Testimonial key={index}>
+                <ClientAvatar>
+                  <img src={image} alt="avatar1" />
+                </ClientAvatar>
+                <h5>{name}</h5>
+                <small>
+                  {description}
+                </small>
+              </Testimonial>
+            )
+          })
+        }
+      </TestimonialsContainer>
+    </TestimonialsSection>
   )
 }
 
--- src/components/Testimonials/styles.js
+++ src/components/Testimonials/styles.js
@@ -1,6 +1,50 @@
 import styled from "styled-components";
 
-const Div = styled.div`
+export const TestimonialsSection = styled.section`
 
 `
-export default Div;
+
+export const TestimonialsContainer = styled.div`
+    width: 40%;
+    
+    @media screen and (max-width: 1024px) {
+        width:60%;
+    }
+
+    @media screen and (max-width: 600px) {
+        width: var(--container-width-sm);
+    }
+`
+
+export const Testimonial = styled.article`
+    background: var(--color-bg-variant);
+    text-align: center;
+    padding: 2rem;
+    border-radius: 2rem;
+    user-select: none;
+
+    small {
+        color: var(--color-light);
+        font-weight: 300;
+        display: block;
+        width: 80%;
+        margin: 0.8rem auto 0;
+    }
+
+    @media screen and (max-width: 1024px) {
+        
+    }
+
+    @media screen and (max-width: 600px) {
+        width: var(--container-width-sm);
+    }
+`
+
+export const ClientAvatar = styled.div`
+    width: 4rem;
+    aspect-ratio: 1/1;
+    overflow: hidden;
+    border-radius: 50%;
+    margin: 0 auto 1rem;
+    border: 0.4rem solid var(--color-primary-variant);
+`
